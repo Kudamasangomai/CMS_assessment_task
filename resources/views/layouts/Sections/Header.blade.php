dc45d838ccd8aba183bcc6364337b0fda26c78d9
Kudamasangomai <Kudam775@gmail.com>
2024-06-22 23:37:53 +0200

    initial commit

---
+++ resources/views/layouts/Sections/Header.blade.php
@@ -0,0 +1,64 @@
+<header class="header navbar-area">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <div class="nav-inner">
+                    <!-- Start Navbar -->
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="assets/images/logo/white-logo.svg" alt="Logo">
+                        </a>
+                        <button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="toggler-icon"></span>
+                            <span class="toggler-icon"></span>
+                            <span class="toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                            <ul id="nav" class="navbar-nav ms-auto">
+                                <li class="nav-item">
+                                    <a href="#home" class="page-scroll active"
+                                        aria-label="Toggle navigation">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#features" class="page-scroll"
+                                        aria-label="Toggle navigation">Features</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="javascript:void(0)" aria-label="Toggle navigation">Overview</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#pricing" class="page-scroll"
+                                        aria-label="Toggle navigation">Pricing</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="javascript:void(0)" aria-label="Toggle navigation">Team</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
+                                        data-bs-target="#submenu-1-4" aria-controls="navbarSupportedContent"
+                                        aria-expanded="false" aria-label="Toggle navigation">Blog</a>
+                                    <ul class="sub-menu collapse" id="submenu-1-4">
+                                        <li class="nav-item"><a href="javascript:void(0)">Blog Grid Sidebar</a>
+                                        </li>
+                                        <li class="nav-item"><a href="javascript:void(0)">Blog Single</a></li>
+                                        <li class="nav-item"><a href="javascript:void(0)">Blog Single
+                                                Sibebar</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="javascript:void(0)" aria-label="Toggle navigation">Contact</a>
+                                </li>
+                            </ul>
+                        </div> <!-- navbar collapse -->
+                        <div class="button add-list-button">
+                            <a href="javascript:void(0)" class="btn">Get it now</a>
+                        </div>
+                    </nav>
+                    <!-- End Navbar -->
+                </div>
+            </div>
+        </div> <!-- row -->
+    </div> <!-- container -->
+</header>
